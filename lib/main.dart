50a8518a428e0695442e39ec39d55e95e91a200c
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-31 01:20:30 -0300

    removing provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:movies/domain/repository/movie_repository.dart';
-import 'package:movies/view/pages/introduction_page.dart';
-import 'package:provider/provider.dart';
+import 'package:movies/view/pages/introduction/introduction_page.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => MovieRepository()),
-      ],
-      child: const MyApp(),
-    ),
+    const MyApp(),
   );
 }
 
