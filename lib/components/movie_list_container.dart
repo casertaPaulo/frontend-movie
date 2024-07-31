2dfc84718e2f2001de84ef1479027104654f4fdd
Paulo Henrique <paulo.phc666@gmail.com>
2024-07-31 01:18:01 -0300

    creating container views for movies

--- lib/components/movie_list_container.dart
+++ lib/components/movie_list_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movies/domain/model/movie_entity.dart';
+import 'package:movies/data/model/movie_entity.dart';
 
 class MovieListContainer extends StatelessWidget {
   final MovieEntity movieEntity;
@@ -7,53 +7,55 @@ class MovieListContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        //color: Colors.grey,
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: Image.network(
-              movieEntity.poster,
-              height: 200,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30, left: 20),
-            child: SizedBox(
-              height: 150,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: 150,
-                    child: Text(
-                      textAlign: TextAlign.left,
-                      movieEntity.title,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontFamily: "LemonMilk",
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    movieEntity.year,
-                    style: const TextStyle(
-                        fontSize: 20,
-                        fontFamily: "LemonMilk",
-                        fontWeight: FontWeight.w900),
-                  )
-                ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.network(
+                movieEntity.poster,
+                height: 200,
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 20),
+              child: SizedBox(
+                height: 150,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      child: Text(
+                        textAlign: TextAlign.start,
+                        movieEntity.title,
+                        style: TextStyle(
+                          fontSize: movieEntity.title.length > 30 ? 18 : 23,
+                          fontFamily: "LemonMilk",
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      movieEntity.year,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontFamily: "LemonMilk",
+                          fontWeight: FontWeight.w900),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/components/movie_pageview_container.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:movies/data/model/movie_entity.dart';
+
+class PageViewContainer extends StatelessWidget {
+  final MovieEntity movieEntity;
+  const PageViewContainer({super.key, required this.movieEntity});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        //color: Colors.grey,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(
+              fit: BoxFit.cover,
+              movieEntity.poster,
+              height: MediaQuery.of(context).size.height > 800 ? 350 : 280,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 00),
+            child: Text(
+              textAlign: TextAlign.center,
+              movieEntity.title,
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: "LemonMilk",
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: Text(
+              movieEntity.year,
+              style: const TextStyle(
+                  fontFamily: "LemonMilk",
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
